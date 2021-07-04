03a3b65b191e78bc619d6e0907aa945f2b8791d2
Darolo13 <70618345+Darolo13@users.noreply.github.com>
2021-07-04 07:18:32 -0700

    Add passwordLength if conditional statement

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,5 +23,13 @@ function generatePassword() {
   var passwordLength = prompt("How many characters will your password have? Password must be at least 8 characters and no more than 128 characters.");
 }
 
+passwordLength = parseInt(passwordLength);
+
+ // makes user input a length between 8 and 128 
+if (passwordLength < 8 || passwordLength > 128) {
+  alert("Password must have more than 8 and less than 128 characters!");
+  return generatePassword(); 
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
